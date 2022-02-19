3314362cc1faf4bc1600fc31fa28a8d06f32502f
sfillipi <sfillipi@mail.bradley.edu>
2022-02-19 15:13:25 -0600

    part 2 proto

--- Prototype_03/Assets/Scripts/PlayerController.cs
+++ Prototype_03/Assets/Scripts/PlayerController.cs
@@ -13,12 +13,31 @@ public class PlayerController : MonoBehaviour
     public bool isOnGround = true;
     public bool gameOver = false;
 
+    public Animator playerAnimator;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+
+    private AudioSource playerAudio;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         //set a reference to the rigidbody component
         rb = GetComponent<Rigidbody>();
+
+        //set a reference to ani component
+        playerAnimator = GetComponent<Animator>();
+
+        //set reference to audio source
+        playerAudio = GetComponent<AudioSource>();
+
+        //start running animation on start
+        playerAnimator.SetFloat("Speed_f", 1.0f);
 
         forceMode = ForceMode.Impulse;
 
@@ -34,10 +53,20 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // jump when space is pressed
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             rb.AddForce(Vector3.up * jumpForce, forceMode);
             isOnGround = false;
+
+            //set the trigger for jump ani
+            playerAnimator.SetTrigger("Jump_trig");
+
+            //stop playing dirt particle on jump
+            dirtParticle.Stop();
+
+            //play jump sfx
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
 
 
@@ -48,11 +77,27 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+
+            //play dirt ani when player is on ground
+            dirtParticle.Play();
         }
         else if(collision.gameObject.CompareTag("Obstacle"))
         {
             Debug.Log("Game Over!");
             gameOver = true;
+
+            //play death ani
+            playerAnimator.SetBool("Death_b", true);
+            playerAnimator.SetInteger("DeathType_int", 1);
+
+            //stop dirt
+            dirtParticle.Stop();
+
+            //play explosion
+            explosionParticle.Play();
+
+            //play crash sfx
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
 }
