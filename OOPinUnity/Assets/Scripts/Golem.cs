ff32a99741034bfce1bd5ae904f5fac9977ea78b
sfillipi <sfillipi@mail.bradley.edu>
2022-04-03 15:07:08 -0500

    first part of video

---
+++ OOPinUnity/Assets/Scripts/Golem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Golem : Enemy
+{
+
+    protected int damage;
+
+    // Start is called before the first frame update
+    protected override void Awake()
+    {
+        base.Awake();
+        health = 120;
+
+    }
+
+    protected override void Attack()
+    {
+        Debug.Log("Golem attacks!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void TakeDamage(int amount)
+    {
+        Debug.Log("You took " + amount + " points of damage!");
+    }
+}
---
+++ OOPinUnity/Assets/Scripts/Weapon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public int damageBonus;
+
+
+    public void Recharge()
+    {
+        Debug.Log("Recharging Weapon!");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
